4604f69cdc44c9f5cb78572c653596d7ee614e13
Hackerl <490021209@qq.com>
2020-11-30 21:38:33 +0800

    Update#jsoncpp new version

--- build-jsoncpp.sh
+++ build-jsoncpp.sh
@@ -4,7 +4,7 @@ rootdir=$(cd "$(dirname "$0")"; pwd)
 set -e
 
 # uncompress
-[ ! -d $rootdir/jsoncpp ] && tar -xzvf jsoncpp-0.10.7.tar.gz && mv jsoncpp-0.10.7 jsoncpp
+[ ! -d $rootdir/jsoncpp ] && tar -xzvf jsoncpp-1.9.4.tar.gz && mv jsoncpp-1.9.4 jsoncpp
 
 # build
 cd $rootdir/jsoncpp; mkdir -p cmake-build; cd cmake-build
